fe3786ffd62ee3ac9a12e3322a17fe7d6742a1dd
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2025-02-04 18:52:45 -0300

    Quinto commit
    Estudando props e destructuring props no React

--- 3 - AVANÇANDO EM REACT/avancando/src/App.jsx
+++ 3 - AVANÇANDO EM REACT/avancando/src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css'
+import "./App.css";
 
-import Img2 from './assets/img2.jpg'
-import CondicionalRender from './components/CondicionalRender'
-import ListRender from './components/ListRender'
-import ManageData from './components/ManageData'
+import Img2 from "./assets/img2.jpg";
+import CondicionalRender from "./components/CondicionalRender";
+import ListRender from "./components/ListRender";
+import ManageData from "./components/ManageData";
+import ShowUserName from "./components/ShowUserName";
 
 function App() {
-
-
   return (
     <>
-      <div className='App'>
+      <div className="App">
         <h1>Avançando em React</h1>
         <div>
           <img src="/img1.jpg" alt="Gatinho fofo" />
@@ -18,12 +17,16 @@ function App() {
         <div>
           <img src={Img2} alt="Outro gatinho fofo" />
         </div>
-        <ManageData/>
-        <ListRender/>
-        <CondicionalRender/>
+        <ManageData />
+        <ListRender />
+        <CondicionalRender />
+        {/*props*/}
+        <ShowUserName name="Diogo" />
+        {/*destructuing props*/}
+        <CarDetails brand="VW" km={100000} color="Azul" />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
