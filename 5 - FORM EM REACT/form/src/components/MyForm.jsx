f90a0a0967723263ad387313cdeaa7b041827d8d
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2025-02-20 18:32:39 -0300

    Décimo quinto
    Estudando manipulação de dados em forms

--- 5 - FORM EM REACT/form/src/components/MyForm.jsx
+++ 5 - FORM EM REACT/form/src/components/MyForm.jsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
 import "./MyForm.css";
 
 const MyForm = () => {
+  //3 -  gerenciamento de dados
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
+  console.log(email);
+
   return (
     <div>
       {/* 1 - Criação de form */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite o seu nome" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+          />
         </div>
         {/* 2 - label envolvendo o input */}
         <label>
           <span>Email:</span>
-          <input type="email" name="email" placeholder="Digite o seu email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <input type="submit" value="Enviar" />
       </form>
