6c937294ea8ef20bda066425c4accce0390439e2
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2025-02-20 13:39:22 -0300

    Décimo quarto commit
    Label envolvendo input

--- 5 - FORM EM REACT/form/src/components/MyForm.jsx
+++ 5 - FORM EM REACT/form/src/components/MyForm.jsx
@@ -9,6 +9,11 @@ const MyForm = () => {
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder="Digite o seu nome" />
         </div>
+        {/* 2 - label envolvendo o input */}
+        <label>
+          <span>Email:</span>
+          <input type="email" name="email" placeholder="Digite o seu email" />
+        </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>
