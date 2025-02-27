2a2661964017eaccd6425f54d5e0b4bd7dffffc7
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2025-02-27 19:01:45 -0300

    Décimo sexto commit
    Envio de form, controlled input e limpar formulario

--- 5 - FORM EM REACT/form/src/App.jsx
+++ 5 - FORM EM REACT/form/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import MyForm from "./components/MyForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h2>Forms</h2>
-      <MyForm />
+      <MyForm user={{ name: "Maria", email: "maria@gmail.com" }} />
     </>
   );
 }
--- 5 - FORM EM REACT/form/src/components/MyForm.jsx
+++ 5 - FORM EM REACT/form/src/components/MyForm.jsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
 import "./MyForm.css";
+import PropTypes from "prop-types";
 
-const MyForm = () => {
+const MyForm = ({ user = {} }) => {
+  // 6 -  controlled inputs
   //3 -  gerenciamento de dados
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState(user ? user.name : "");
+  const [email, setEmail] = useState(user ? user.email : "");
+
+  console.log("User recebido", user);
 
   const handleName = (e) => {
     setName(e.target.value);
   };
 
-  console.log(name);
-  console.log(email);
+  //console.log(name);
+  //console.log(email);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Enviando o formulário");
+    console.log(name, email);
+  };
+
+  //7 -  limpar formulário
+  setName("");
+  setEmail("");
 
   return (
     <div>
+      {/* 5 - envio de form */}
       {/* 1 - Criação de form */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nome:</label>
           <input
@@ -24,22 +39,32 @@ const MyForm = () => {
             name="name"
             placeholder="Digite o seu nome"
             onChange={handleName}
+            value={name}
           />
         </div>
         {/* 2 - label envolvendo o input */}
         <label>
           <span>Email:</span>
+          {/* 4 - Simplificação de manipulação de state*/}
           <input
             type="email"
             name="email"
             placeholder="Digite o seu email"
             onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>
   );
+};
+
+MyForm.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+  }),
 };
 
 export default MyForm;
